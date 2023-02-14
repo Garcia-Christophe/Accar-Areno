78c95b8554b14561b1b60ab2e2ed64f790fabefb
christophe.garci <christophe.garci@depinfoad.univ-brest.fr>
2023-02-14 09:56:24 +0100

    add pojo JPA

---
+++ src/data/Groupe.java
@@ -0,0 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package data;
+
+import java.io.Serializable;
+import java.util.Set;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author christophe.garci
+ */
+@Entity
+@Table(name = "Groupe")
+@NamedQueries({
+    @NamedQuery(name = "Groupe.findAll", query = "SELECT g FROM Groupe g"),
+    @NamedQuery(name = "Groupe.findByIdGroupe", query = "SELECT g FROM Groupe g WHERE g.idGroupe = :idGroupe"),
+    @NamedQuery(name = "Groupe.findByNom", query = "SELECT g FROM Groupe g WHERE g.nom = :nom"),
+    @NamedQuery(name = "Groupe.findByNbArtists", query = "SELECT g FROM Groupe g WHERE g.nbArtists = :nbArtists")})
+public class Groupe implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idGroupe")
+    private Integer idGroupe;
+    @Column(name = "nom")
+    private String nom;
+    @Column(name = "nbArtists")
+    private Integer nbArtists;
+    @JoinColumn(name = "idArtiste", referencedColumnName = "idArtiste")
+    @ManyToOne
+    private Artiste idArtiste;
+    @OneToMany(mappedBy = "idGroupe")
+    private Set<Concert> concertSet;
+
+    public Groupe() {
+    }
+
+    public Groupe(Integer idGroupe) {
+        this.idGroupe = idGroupe;
+    }
+
+    public Integer getIdGroupe() {
+        return idGroupe;
+    }
+
+    public void setIdGroupe(Integer idGroupe) {
+        this.idGroupe = idGroupe;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Integer getNbArtists() {
+        return nbArtists;
+    }
+
+    public void setNbArtists(Integer nbArtists) {
+        this.nbArtists = nbArtists;
+    }
+
+    public Artiste getIdArtiste() {
+        return idArtiste;
+    }
+
+    public void setIdArtiste(Artiste idArtiste) {
+        this.idArtiste = idArtiste;
+    }
+
+    public Set<Concert> getConcertSet() {
+        return concertSet;
+    }
+
+    public void setConcertSet(Set<Concert> concertSet) {
+        this.concertSet = concertSet;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idGroupe != null ? idGroupe.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Groupe)) {
+            return false;
+        }
+        Groupe other = (Groupe) object;
+        if ((this.idGroupe == null && other.idGroupe != null) || (this.idGroupe != null && !this.idGroupe.equals(other.idGroupe))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "data.Groupe[ idGroupe=" + idGroupe + " ]";
+    }
+    
+}
